e469edc3dea804f20b2605afc4939d3c81055453
oussouss <tahiri.oussama@gmail.com>
2020-02-06 09:50:20 +0100

    Upgrade from spring 2.1.4 to 2.2.9

--- src/main/java/fr/otahiri/spring/divers/SpringDiversApplication.java
+++ src/main/java/fr/otahiri/spring/divers/SpringDiversApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDiversApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDiversApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiversApplication.class, args);
+    }
 }
